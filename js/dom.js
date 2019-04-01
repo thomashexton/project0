011fd21e9d2607ad330325398349ce96ca677f81
Thomas Hexton <tom.hexton@gmail.com>
2019-04-01 12:04:35 +1100

    setup file structure

---
+++ js/dom.js
@@ -0,0 +1,3 @@
+	const render = function() {
+		// if one of the value equals 1 or 2 then load in correspnding image
+	};
---
+++ js/logic.js
@@ -0,0 +1,12 @@
+const board = {
+	topLeft: 0,
+	topMiddle: 0,
+	topRight: 0,
+	midLeft: 0,
+	midMiddle: 0,
+	midRight: 0,
+	botLeft: 0,
+	botMiddle: 0,
+	botRight: 0,
+
+};
