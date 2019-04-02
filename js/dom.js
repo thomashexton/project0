775830caab1ce3439a800968d83c91bc3ff6386e
Thomas Hexton <tom.hexton@gmail.com>
2019-04-02 19:04:27 +1100

    pushed for backup, still broken

--- js/dom.js
+++ js/dom.js
@@ -15,28 +15,57 @@ $( document ).ready( function() {
 
 	var lastRenderValue = 0;
 
-	// on page load, add data attributes to jQuery .cell objects
+	// on page load, add data render attributes to jQuery '.cell' objects
 	$( '.cell' ).data( 'render', 0 );
 
 	$( '.cell' ).on( 'click', function() {
-		// first click place desired action
+		// first click places an X (1)
 		if ( lastRenderValue === 0 ) {
+			// change the jQuery object data-row='1'
 			$( this ).data( 'render', 1 );
+
+			const whichBoard = $( this ).data( 'render' );
+			const whichKey = $( this ).data( 'row' );
+			const whichCell = $( this ).data( 'cell' );
+
+			game.addToBoard( whichBoard, whichKey, whichCell );
+
 			lastRenderValue = 2;
+
 		} // so they can't reclick a used square
 		else if ( $( this ).data( 'render' ) !== 0 ) {
 			console.log( 'Can\'t reselect.' );
+
 		} // if previous marker placed was X (1) choose O (2)
 		else if ( lastRenderValue === 1 ) {
 			$( this ).data( 'render', 1 );
+
+			const whichBoard = $( this ).data( 'render' );
+			const whichKey = $( this ).data( 'row' );
+			const whichCell = $( this ).data( 'cell' );
+
+			// console.log(whichBoard, whichKey, whichCell, '*');
+
+			game.addToBoard( whichBoard, whichKey, whichCell );
+
 			lastRenderValue = 2;
+
 		} // if previous marker placed was O (2) choose X (1)
 		else if ( lastRenderValue === 2 ) {
 			$( this ).data( 'render', 2 );
+
+			const whichBoard = $( this ).data( 'render' );
+			const whichKey = $( this ).data( 'row' );
+			const whichCell = $( this ).data( 'cell' );
+
+			// console.log(whichBoard, whichKey, whichCell, '**');
+
+			game.addToBoard( whichBoard, whichKey, whichCell );
+
 			lastRenderValue = 1;
 		}
-		// console.log($(this).data('row'), $(this).data('cell'));
-		// console.log( lastRenderValue );
+
+		// console.log( $( this ).data( 'row' ), $( this ).data( 'cell' ) );
 		render();
 	} );
 } );
--- js/logic.js
+++ js/logic.js
@@ -1,22 +1,61 @@
 const game = {
-	board: {
-		foo: [ '', '', '' ],
-		oof: [ '', '', '' ],
-		ofo: [ '', '', '' ]
+	boardX: {
+		'0': [ true, true, true ],
+		'1': [ ],
+		'2': [ ]
 	},
+
+	boardO: {
+		'0': [ ],
+		'1': [ ],
+		'2': [ ]
+	},
+
 	winningCombos: [
-		[ 00, 01, 02 ],
-		[ 10, 11, 12 ],
-		[ 20, 21, 22 ],
-		[ 00, 10, 20 ],
-		[ 01, 11, 21 ],
-		[ 02, 12, 22 ],
-		[ 00, 11, 22 ],
-		[ 02, 11, 20 ]
+		[ [0, 0], [0, 1], [0, 2] ], // [0]
+		[ [1, 0], [1, 1], [1, 2] ], // [1]
+		[ [2, 0], [2, 1], [2, 2] ], // [2]
+		[ [0, 0], [1, 0], [2, 0] ], // [3]
+		[ [0, 1], [1, 1], [2, 1] ], // [4]
+		[ [0, 2], [1, 2], [2, 2] ], // [5]
+		[ [0, 0], [1, 1], [2, 2] ], // [6]
+		[ [0, 2], [1, 1], [2, 0] ]  // [7]
 	],
+
+	addToBoard: function( whichBoard, whichKey, whichCell ) {
+		// if boardX then put
+		if (whichBoard === 1) {
+			// add to boardX
+			this.boardX[whichKey].push( true );
+			this.scanForMatch();
+		} else if (whichBoard === 2) {
+			// add to boardO
+			this.boardO[whichKey].push( true );
+			this.scanForMatch();
+		}
+	},
+
+	scanForMatch: function() {
+		for ( let i = 0; i < this.winningCombos.length; i += 1 ) {
+
+			const combos = this.winningCombos[ i ]
+
+			// console.log( combos );
+
+			const x0 = combos[0][0];
+			const y0 = combos[0][1];
+			const x1 = combos[1][0];
+			const y1 = combos[1][1];
+			const x2 = combos[2][0];
+			const y2 = combos[2][1];
+
+
+			// if ( this.boardX[ x0 ][ y0 ] === true &&
+			// 	 this.boardX[ x1 ][ y1 ] === true &&
+			// 	 this.boardX[ x2 ][ y2 ] === true) {
+			// 		 console.log('Winner');
+			// 		 break;
+			// }
+		}
+	}
 };
-
-
-// need to work out how to alternate between X and O
-
-//run sort on the arrays & if they match the winning arrays, game over
