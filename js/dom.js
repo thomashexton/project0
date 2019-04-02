c6578a317ccb61acf3894ba925932359f48e33c9
Thomas Hexton <tom.hexton@gmail.com>
2019-04-02 22:10:41 +1100

    working win algorythm

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,9 @@
 const render = function() {
 	// iterate over each div and find if it's data value is equal to X or O ... replacing it as necessary
 	$( '.cell' ).each( function() {
-		if ( $( this ).data( 'render' ) === 1 ) {
+		if ( $( this ).data( 'render' ) === 'X' ) {
 			$( this ).html( '<img src="img/cross.svg">' )
-		} else if ( $( this ).data( 'render' ) === 2 ) {
+		} else if ( $( this ).data( 'render' ) === 'O' ) {
 			$( this ).html( '<img src="img/nought.svg">' )
 		} else {
 			$( this ).html( '<img src="img/blank.svg">' )
@@ -19,50 +19,46 @@ $( document ).ready( function() {
 	$( '.cell' ).data( 'render', 0 );
 
 	$( '.cell' ).on( 'click', function() {
-		// first click places an X (1)
+		// first click places an X
 		if ( lastRenderValue === 0 ) {
 			// change the jQuery object data-row='1'
-			$( this ).data( 'render', 1 );
+			$( this ).data( 'render', 'X' );
 
-			const whichBoard = $( this ).data( 'render' );
+			const whichMarker = $( this ).data( 'render' );
 			const whichKey = $( this ).data( 'row' );
 			const whichCell = $( this ).data( 'cell' );
 
-			game.addToBoard( whichBoard, whichKey, whichCell );
+			game.addToBoard( whichMarker, whichKey, whichCell );
 
-			lastRenderValue = 2;
+			lastRenderValue = 'X';
 
 		} // so they can't reclick a used square
 		else if ( $( this ).data( 'render' ) !== 0 ) {
 			console.log( 'Can\'t reselect.' );
 
 		} // if previous marker placed was X (1) choose O (2)
-		else if ( lastRenderValue === 1 ) {
-			$( this ).data( 'render', 1 );
+		else if ( lastRenderValue === 'X' ) {
+			$( this ).data( 'render', 'O' );
 
-			const whichBoard = $( this ).data( 'render' );
+			const whichMarker = $( this ).data( 'render' );
 			const whichKey = $( this ).data( 'row' );
 			const whichCell = $( this ).data( 'cell' );
 
-			// console.log(whichBoard, whichKey, whichCell, '*');
+			game.addToBoard( whichMarker, whichKey, whichCell );
 
-			game.addToBoard( whichBoard, whichKey, whichCell );
-
-			lastRenderValue = 2;
+			lastRenderValue = 'O';
 
 		} // if previous marker placed was O (2) choose X (1)
-		else if ( lastRenderValue === 2 ) {
-			$( this ).data( 'render', 2 );
+		else if ( lastRenderValue === 'O' ) {
+			$( this ).data( 'render', 'X' );
 
-			const whichBoard = $( this ).data( 'render' );
+			const whichMarker = $( this ).data( 'render' );
 			const whichKey = $( this ).data( 'row' );
 			const whichCell = $( this ).data( 'cell' );
 
-			// console.log(whichBoard, whichKey, whichCell, '**');
+			game.addToBoard( whichMarker, whichKey, whichCell );
 
-			game.addToBoard( whichBoard, whichKey, whichCell );
-
-			lastRenderValue = 1;
+			lastRenderValue = 'X';
 		}
 
 		// console.log( $( this ).data( 'row' ), $( this ).data( 'cell' ) );
--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,8 @@
 const game = {
-	boardX: {
-		'0': [ true, true, true ],
-		'1': [ ],
-		'2': [ ]
-	},
-
-	boardO: {
-		'0': [ ],
-		'1': [ ],
-		'2': [ ]
+	board: {
+		'0': [ null, null, null ],
+		'1': [ null, null, null ],
+		'2': [ null, null, null ]
 	},
 
 	winningCombos: [
@@ -22,40 +16,66 @@ const game = {
 		[ [0, 2], [1, 1], [2, 0] ]  // [7]
 	],
 
-	addToBoard: function( whichBoard, whichKey, whichCell ) {
+	addToBoard: function( whichMarker, whichKey, whichCell ) {
 		// if boardX then put
-		if (whichBoard === 1) {
+		if ( whichMarker === 'X' ) {
 			// add to boardX
-			this.boardX[whichKey].push( true );
+			if ( whichCell === 0 ) {
+				this.board[ whichKey ].splice( 0, 1, 'X' );
+
+			} else if ( whichCell === 1 ) {
+				this.board[ whichKey ].splice( 1, 1, 'X' );
+
+			} else if ( whichCell === 2 ) {
+				this.board[ whichKey ].splice( 2, 1, 'X' );
+
+			}
+
 			this.scanForMatch();
-		} else if (whichBoard === 2) {
+
+		} else if ( whichMarker === 'O' ) {
 			// add to boardO
-			this.boardO[whichKey].push( true );
+			if ( whichCell === 0 ) {
+				this.board[ whichKey ].splice( 0, 1, 'O' );
+
+			} else if ( whichCell === 1 ) {
+				this.board[ whichKey ].splice( 1, 1, 'O' );
+
+			} else if ( whichCell === 2 ) {
+				this.board[ whichKey ].splice( 2, 1, 'O' );
+
+			}
+
 			this.scanForMatch();
 		}
 	},
 
 	scanForMatch: function() {
+
 		for ( let i = 0; i < this.winningCombos.length; i += 1 ) {
 
 			const combos = this.winningCombos[ i ]
 
-			// console.log( combos );
+			const x0 = this.winningCombos[ i ][ 0 ][ 0 ];
+			const y0 = this.winningCombos[ i ][ 0 ][ 1 ];
+			const x1 = this.winningCombos[ i ][ 1 ][ 0 ];
+			const y1 = this.winningCombos[ i ][ 1 ][ 1 ];
+			const x2 = this.winningCombos[ i ][ 2 ][ 0 ];
+			const y2 = this.winningCombos[ i ][ 2 ][ 1 ];
 
-			const x0 = combos[0][0];
-			const y0 = combos[0][1];
-			const x1 = combos[1][0];
-			const y1 = combos[1][1];
-			const x2 = combos[2][0];
-			const y2 = combos[2][1];
+			// console.log( x2, y2 );
+			// console.log( this.board[ x2 ][ y2 ] );
 
-
-			// if ( this.boardX[ x0 ][ y0 ] === true &&
-			// 	 this.boardX[ x1 ][ y1 ] === true &&
-			// 	 this.boardX[ x2 ][ y2 ] === true) {
-			// 		 console.log('Winner');
-			// 		 break;
-			// }
+			if ( this.board[ x0 ][ y0 ] === 'X' && this.board[ x1 ][ y1 ] === 'X' && this.board[ x2 ][ y2 ] === 'X') {
+				console.log( 'Winner' );
+				break;
+			} // check for O
+			else if ( this.board[ x0 ][ y0 ] === 'O' && this.board[ x1 ][ y1 ] === 'O' && this.board[ x2 ][ y2 ] === 'O' ) {
+				 console.log( 'Winner' );
+		  		break;
+			} else if () {
+				console.log('draw');
+			}
 		}
 	}
 };
